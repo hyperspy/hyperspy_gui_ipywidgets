542e7516119902823fa4d6d0c5ef13cf548b5456
Francisco de la Peña <frandelapena@gmail.com>
2017-05-16 17:50:04 +0100

    Register hyperspy toolkeys

--- hyperspy_gui_ipywidgets/__init__.py
+++ hyperspy_gui_ipywidgets/__init__.py
@@ -1,6 +1,6 @@
 
 __version__ = 1.3
-
+import hyperspy.api_nogui # necessary to register the toolkeys
 # Importing as below registers the widgets when importing the module
 import hyperspy_gui_ipywidgets.axes
 import hyperspy_gui_ipywidgets.model
