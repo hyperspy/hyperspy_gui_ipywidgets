1c2954f41414b4a8fe07208897bfd987d00e61b2
Francisco de la Peña <frandelapena@gmail.com>
2017-05-24 15:27:22 +0100

    Release 1.0

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.0.0.rc1',
+    version='1.0.0',
 
     description=('ipywidgets GUI elements for HyperSpy.'),
     long_description=long_description,
@@ -67,7 +67,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['hyperspy', 'ipywidgets>=6.0', 'link_traits'],
+    install_requires=['hyperspy>=1.3', 'ipywidgets>=6.0', 'link_traits'],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
