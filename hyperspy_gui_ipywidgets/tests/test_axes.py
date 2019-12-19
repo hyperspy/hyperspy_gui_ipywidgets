1fd8f7277ef3fcd8c86d2dba39c46221b656ef6b
jlaehne <jonas@pdi-berlin.de>
2019-12-19 23:56:07 +0100

    Added test

--- hyperspy_gui_ipywidgets/tests/test_axes.py
+++ hyperspy_gui_ipywidgets/tests/test_axes.py
@@ -65,3 +65,26 @@ class TestAxes:
                               attributes=("value", "index", "units",
                                           "index_in_array", "name",
                                           "size", "scale", "offset"))
+                                          
+class TestNonLinearAxes:
+
+    def setup_method(self, method):
+        dict0 = {'scale': 1.0}
+        dict1 = {'expression': 'a / (x+1)', 'a': 1240, 'size': 3, 
+                 'name': 'plumage', 'units': 'beautiful', }
+        dict2 = {'axis': np.arange(4), 'name': 'norwegianblue', 'units': 'ex', 
+                 'navigate': False, }
+        self.s = hs.signals.Signal1D(np.empty((2, 3, 4)), axes=[dict0, dict1,
+                                                                dict2])
+        self.s.axes_manager[0].navigate = False
+
+    def test_axes_manager_gui(self):
+        s = self.s
+        am = self.s.axes_manager
+        wd = s.axes_manager.gui(**KWARGS)["ipywidgets"]["wdict"]
+        check_axis_attributes(axes_manager=am, widgets_dict=wd, index=1,
+                              attributes=("name", "units", "size",
+                                          "index_in_array"))
+        check_axis_attributes(axes_manager=am, widgets_dict=wd, index=2,
+                              attributes=("name", "units", "size",
+                                          "index_in_array"))
