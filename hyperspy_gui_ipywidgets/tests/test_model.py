4817c129de5f20b7f59b9f744f746b2c91a8ee44
Eric Prestat <eric.prestat@gmail.com>
2021-03-09 13:03:49 +0000

    Increase coverage.

--- hyperspy_gui_ipywidgets/tests/test_model.py
+++ hyperspy_gui_ipywidgets/tests/test_model.py
@@ -35,7 +35,8 @@ def test_multivalue_parameter():
     wd["element1"]["value"].value = -3
     assert wd["element0"]["value"].value == p.value[0]
     assert wd["element1"]["value"].value == p.value[1]
-    # TODO: update button
+
+    wd["update_button"]._click_handlers(wd["update_button"])    # Trigger it
     # TODO: bounds
 
 
@@ -132,4 +133,6 @@ def test_fit_component():
     assert wd["iterpath"].disabled == True
     fc.only_current = False
     assert wd["iterpath"].disabled == False
+
+    wd["close_button"]._click_handlers(wd["close_button"])    # Trigger it
 
